1dbb30ca96620b995fded76d90d45c01cc13b73e
林帆 <flin@thoughtworks.com>
2017-04-09 00:16:23 +0800

    simplify image reading

--- extract_image.py
+++ extract_image.py
@@ -14,14 +14,10 @@ if not os.path.exists(out_dir):
 i = 0
 for topic, msg, time in rbag.read_messages():
     if topic == '/image_raw':
-        byte = [elem.encode("hex") for elem in msg.data]  # string to byte array
-        raw_data = [int(n, 16) for n in byte]      # byte array to int array
-        reshape_raw_data = np.reshape(raw_data, (512, 1400)).astype('uint8')  # reshape to 2-dim array
-        im = Image.fromarray(reshape_raw_data)     # monochromatic image
-        im_rgb = Image.merge('RGB', (im, im, im))  # color image
-        im_rgb.save('%s/%s.png' % (out_dir, i))    # save image
+        im = Image.frombytes('L', (1400, 512), msg.data)
+        im.save('%s/%s.png' % (out_dir, i))
         i += 1
         print('Extracted %s images' % i)
-        if i >= 20:
+        if i >= 10:
             break
 rbag.close()
